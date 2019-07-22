72571b01c14f01f310dcea97e3e618bfe4d4490b
neoe <neoedmund@gmail.com>
2019-07-22 10:52:24 +0800

    change to new plugin interface

---
+++ src/neoe/ne/PluginInit.java
@@ -0,0 +1,41 @@
+package neoe.ne;
+
+import static neoe.ne.U.Config.getConfig;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+import neoe.ne.Ime.ImeInterface;
+
+public class PluginInit {
+	public static void run() throws IOException {
+		Map config = getConfig();
+		List list = (List) config.get("ime");
+		if (list == null || list.size() == 0) {
+			return;
+		}
+		List<ImeInterface> imes = new ArrayList();
+		for (Object o : list) {
+			String cls = (String) o;
+			try {
+				Class clz = PluginInit.class.getClassLoader().loadClass(cls);
+				if (!ImeInterface.class.isAssignableFrom(clz)) {
+					System.out.println("IME class '" + cls + "' not implements 'ImeInterface'.");
+					continue;
+				}
+				ImeInterface ime = null;
+				try {
+					imes.add(ime = (ImeInterface) clz.getConstructor().newInstance());
+					System.out.println("add IME:" + ime.getImeName());
+				} catch (Exception ex) {
+					System.out.println("IME class '" + cls + "' cannot be inited:" + ex);
+				}
+			} catch (ClassNotFoundException ex) {
+				System.out.println("IME class not found:" + cls);
+			}
+		}
+		Ime.instances.addAll(imes);
+	}
+}
